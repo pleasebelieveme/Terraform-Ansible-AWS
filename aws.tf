059027fa88d94b93ea9dd3576e89bdff7626595e
pleasebelieveme <146408987+pleasebelieveme@users.noreply.github.com>
2023-11-29 10:34:18 +0900

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -5,41 +5,51 @@ provider "aws" {
 
 # VPC 리소스 생성
 resource "aws_vpc" "my_vpc" {
-    cidr_block = "172.16.0.0/16" # VPC의 IP 주소 범위 설정
+    # VPC의 IP 주소 범위 설정
+    cidr_block = "172.16.0.0/16" 
+    # VPC에 이름 설정
     tags = {
-        Name = "myVPC" # VPC에 이름 설정
+        Name = "myVPC" 
     }
 }
 
 # Subnet 리소스 생성
 resource "aws_subnet" "my_subnet" {
     vpc_id            = aws_vpc.my_vpc.id
-    cidr_block        = "172.16.10.0/24" # Subnet의 IP 주소 범위 설정
-    availability_zone = "ap-northeast-2a" # 사용할 가용 영역 설정
+    # Subnet의 IP 주소 범위 설정
+    cidr_block        = "172.16.10.0/24" 
+    # 사용할 가용 영역 설정
+    availability_zone = "ap-northeast-2a" 
+    # Subnet에 이름 설정
     tags = {
-        Name = "mySubnet" # Subnet에 이름 설정
+        Name = "mySubnet" 
     }
 }
 
 # Network Interface 리소스 생성
 resource "aws_network_interface" "my_net" {
     subnet_id   = aws_subnet.my_subnet.id
-    private_ips = ["172.16.10.100"] # Network Interface에 할당할 IP 주소 설정
+    # Network Interface에 할당할 IP 주소 설정
+    private_ips = ["172.16.10.100"] 
+    # Network Interface에 이름 설정
     tags = {
-        Name = "private_network_interface" # Network Interface에 이름 설정
+        Name = "private_network_interface" 
     }
 }
 
 # Ubuntu Instance 생성
 resource "aws_instance" "ubuntu" {
-    ami           = "ami-0225bc2990c54ce9a" # Ubuntu 20.04 AMI ID
-    instance_type = "t2.micro" # 인스턴스 유형 설정
+    # Ubuntu 20.04 AMI ID
+    ami           = "ami-0225bc2990c54ce9a" 
+    # 인스턴스 유형 설정
+    instance_type = "t2.micro" 
 
     network_interface {
         network_interface_id = aws_network_interface.my_net.id
         device_index         = 0
     }
+    # 인스턴스에 이름 설정
     tags = {
-        Name = "myUbuntu" # 인스턴스에 이름 설정
+        Name = "myUbuntu" 
     }
 }
